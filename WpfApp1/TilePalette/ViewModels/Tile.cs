0632ba724413ea74fba8009b2b81dc68d18ceef5
Victoria <vica.gensch@gmail.com>
2022-08-11 13:33:04 +0200

    [VORBEREITUNG] OCE Serialisierung

---
+++ WpfApp1/TilePalette/ViewModels/Tile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.TilePalette.ViewModels
+{
+    public class Tile : NotifiableObjectBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        public bool IsWalkable { get; set; }
+        public string MovementCost { get; set; }
+        public string DefenceBonus { get; set; }
+    }
+}
